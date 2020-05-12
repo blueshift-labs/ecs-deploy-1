58b61343cfa6a76cc2a72ed1f414efcf5bf3435e
aniaptebsft <aniruddha.apte@getblueshift.com>
2020-05-12 15:50:28 +0530

    [INF-666] Fix ecs deploy script for mult threaded slack notification

--- ecs_deploy/slack.py
+++ ecs_deploy/slack.py
@@ -93,7 +93,7 @@ class SlackLogger(object):
         pend = primary['pendingCount']
         des = primary['desiredCount']
         primary_message = {
-        "title": f'PRIMARY {service.cluster} / {service.name}',
+        "title": f'PRIMARY <{cluster_link}|{service.cluster}> / <{service_link}|{service.name}>',
         "text": self.progress_bar(run, pend, des) + "\tRunning: %s Pending: %s  Desired: %s" % (run, pend, des)
         }
         attachments = [primary_message]
