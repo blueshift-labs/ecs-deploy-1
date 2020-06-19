08c6cbe092e66a7b14416a1a8924cafe8f51e053
aniaptebsft <aniruddha.apte@getblueshift.com>
2020-06-19 12:00:08 +0530

    Tag latest.

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-    echo "No arguments provided. Arg1 is docker tag name. e.g. 20.5.1"
+    echo "No arguments provided. Arg1 is docker tag name. e.g. 20.6.1"
     exit 1
 fi
 
 TAG=ecs_deploy:$1
+LATEST=ecs_deploy:latest
 
 # Build docker image
 docker build -t $TAG .
@@ -13,8 +14,12 @@ docker build -t $TAG .
 echo "----------- Pushing to prod -> $TAG"
 docker tag $TAG docker-registry.blueshift.vpc/$TAG
 docker push docker-registry.blueshift.vpc/$TAG
+docker tag $TAG docker-registry.blueshift.vpc/$LATEST
+docker push docker-registry.blueshift.vpc/$LATEST
 
 # Tag & push to staging registry
 echo "----------- Pushing to staging -> $TAG"
 docker tag $TAG staging-docker-registry.bsftstaging.vpc/$TAG
 docker push staging-docker-registry.bsftstaging.vpc/$TAG
+docker tag $TAG staging-docker-registry.bsftstaging.vpc/$LATEST
+docker push staging-docker-registry.bsftstaging.vpc/$LATEST
